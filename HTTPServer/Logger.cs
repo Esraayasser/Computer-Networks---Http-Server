f5dbf475ab39ab6df920afdd60bf529b9c303ec0
emanalaa <eman3laa@outlook.com>
2019-12-18 19:40:23 +0200

    Fix shared resources issue.

--- HTTPServer/Logger.cs
+++ HTTPServer/Logger.cs
@@ -3,45 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Threading;
 
 namespace HTTPServer
 {
     class Logger
     {
-        static StreamWriter sr = new StreamWriter("log.txt");
-
+        static Semaphore semaphore = new Semaphore(1,1);
         // Awad
         public static void LogException(Exception ex)
         {
             // TODO: Create log file named log.txt to log exception details in it
             string location = System.Environment.CurrentDirectory + "\\log.txt";
-           /* 
-            if (!File.Exists(location)){
-                FileStream fs;
-                using(fs = new FileStream(location, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
-                {
-                }
-                fs.Close();
-            }
-            */
+           
             //Datetime:
             string dateTime = "Date: " + DateTime.Now.ToString() + "\r\n";
             //message:
             string message ="Message:" + ex.Message;
+
             // for each exception write its details associated with datetime 
-            /*
-            StreamWriter sw = new StreamWriter(location, true);
-            sw.WriteLine(dateTime);
-            sw.WriteLine(message);
-            sw.Close();
-            sw = null;
-            */
+            semaphore.WaitOne();
             using (StreamWriter sw = new StreamWriter(location, true))
             {
                 sw.WriteLine(dateTime);
                 sw.WriteLine(message);
                 sw.Flush();
             }
+            semaphore.Release();
         }
     }
 }
